cd27e33ff79552e0a7bb466cb48df83d4310a06a
baylen doss <baylendoss12@gmail.com>
2022-07-13 13:22:21 -0700

    oops

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -38,8 +38,7 @@ module.exports = (sequelize, DataTypes) => {
       return await User.scope('currentUser').findByPk(user.id);
     }
     static associate(models) {
-      // define association here
-      User.hasMany(models.Song, {foreignKey : 'userId'})
+    
     }
   };
   
